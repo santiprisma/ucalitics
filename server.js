35d4df223917336c2bd1a5a3bc17b1c8b59d0711
Santiago Scalise <671561710@uca.edu.ar>
2017-08-11 20:46:22 -0300

    latest

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb://localhost:27017/airport"
-const express = require("express")
+const uri = 'mongodb://localhost:27017/ucalitics';
+const express = require('express')
 
 MongoClient.connect(uri, (err, db) => {
-	let app = express()
-	let path = require("path")
+	let app = express();
 
-	app.get("/", (req, res)=> {
-		res.sendFile(path.join(__dirname, "/index.html") )
-	})
-	app.use(express.static("public") )
-	app.get("/report", (req, res) => {
-		var options = {
-			"limit": 5
-		};
-
-		db.collection('flights').find({}, options).toArray((err, data) => {
+	app.param('count', (req, res, next, count) => {		
+		db.collection('tracker').find({
+			'type': {
+				$ne: null
+			}
+		}, {
+			'target': 0,
+			'_id': 0
+		}, {
+			'limit': parseInt(count),
+			'sort': [['timestamp', 'desc']]
+		}).toArray((err, data) => {
 			res.send(data);
 		});
-	})
-	app.listen(3000, () =>{
-		console.log("El servidor esta corriendo sobre http://localhost:3000");
+	});
+	
+	app.get('/latest/:count', (req, res) => {
+		res.end();
+	});
+	
+	app.listen(3000, () => {
+		console.log('El servidor esta corriendo sobre http://localhost:3000');
 	});
 });
